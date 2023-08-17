55742f158b3a8214be17154a8dd8d5d7e5d1ed32
Katie Bridson <kbridson@memphis.edu>
2023-08-17 16:18:59 -0500

    Complete freebay app

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: redirect('listings')
+
+  get 'listings', to: 'listings#index', as: 'listings'
+  post 'listings', to: 'listings#create'
+  get 'listings/new', to: 'listings#new', as: 'new_listing'
+  get 'listings/:id', to: 'listings#show', as: 'listing'
+  get 'listings/:id/edit', to: 'listings#edit', as: 'edit_listing'
+  patch 'listings/:id', to: 'listings#update'
+  delete 'listings/:id', to: 'listings#destroy'
 end
